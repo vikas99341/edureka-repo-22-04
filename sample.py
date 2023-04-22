87a146cf280c7d93c64e68f7eec757146ffde27d
vikas99341 <vikasisinlove@gmail.com>
2023-04-22 02:42:21 +0000

    first git rebase

--- sample.py
+++ sample.py
@@ -4,10 +4,15 @@ c=9
 d=1
 e=7
 f=4
+x=
+y=
 
 sum=a+b+c+d+e+f
 sub=a-b
 mul=a*b
 print sum
-print "adding new line from demo branch"
+
+print "adding new line from demo & new  branch"
 print "adding second line from demo branch"
+
+
